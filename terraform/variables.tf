b27519ca3e7f7ccca250e0248974c5478f07080a
Andrey Seregin <auseregin@yandex.ru>
2025-04-22 23:48:05 +0300

    final

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+
+variable "cloud_id" {
+  type        = string
+  default     = "dn233vdl3pdu43936vpg"
+}
+
+variable "folder_id" {
+  type        = string
+  default     = "b1gj6ia0559mol9ufg9k"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+}
